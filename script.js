f067051e455defc7639d230901379495e22f3818
Kelly Ervin <kellyme@vt.edu>
2021-10-12 21:03:42 -0400

    Styling and updated data

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let countyData
 let foodAccessData
 
 let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
 
 let drawMap = () => {
     canvas.selectAll('path')
@@ -13,6 +14,43 @@ let drawMap = () => {
         .append('path')
         .attr('d', d3.geoPath())
         .attr('class', 'county')
+        .attr('fill', (item) => {
+            let fips = item['id']
+            let county = foodAccessData.find((county) => { return county['fips'] === fips; });
+            if (county === undefined) { return 'lightgrey'; }
+            let percentage = county['poverty_%'];
+            if (percentage <= 10) {
+                return 'limegreen';
+            } else if (percentage <= 20) {
+                return 'lightgreen';
+            } else if (percentage <= 30) {
+                return 'gold';
+            } else if (percentage <= 40) {
+                return 'orange';
+            } else { return 'tomato'; }
+        })
+        .attr('data-fips', (item) => {
+            return item['id']
+        })
+        .attr('data-education', (item) => {
+            let fips = item['id']
+            let county = foodAccessData.find((county) => { return county['fips'] === fips; });
+            if (county === undefined) { return 0; }
+            return county['poverty_%'];
+        })
+        .on('mouseover', (countyDataItem) => {
+            tooltip.transition().style('visibility', 'visible');
+            let fips = countyDataItem['id'];
+            let county = foodAccessData.find((county) => { return county['fips'] === fips; });
+            let vartext = "Not Available";
+            if (county === undefined) {
+                tooltip.text('Data not available');
+            } else {
+                tooltip.text(county['name'] + ', ' + county['state'] + ' : ' + county['poverty_%'] + '%');
+                tooltip.attr('poverty-rate', county['poverty_%']);
+            }
+        })
+        .on('mouseout', (countyDataItem) => { tooltip.transition().style('visibility', 'hidden'); });
 }
 
 d3.json(countyURL).then(
@@ -20,7 +58,7 @@ d3.json(countyURL).then(
         if(error) {
             console.log(error);
         } else {
-            countyData = topojson.feature(data, data.objects.counties);
+            countyData = topojson.feature(data, data.objects.counties).features;
             console.log('County Data');
             console.log(countyData);
 
